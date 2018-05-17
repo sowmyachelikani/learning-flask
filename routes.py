6a6855e4b7441ccc6e4e03d7cb93ac6dc4367b71
sowmyachelikani <chelikanisowmya@gmail.com>
2018-05-17 18:27:38 -0500

    About page

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 if __name__=="__main__":
 	app.run(debug=True)
